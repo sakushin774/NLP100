c15afae28610f434e393b06f64b646b03f51c4d8
sakushin <admin@pc-b062.local>
2017-07-28 11:30:44 +0900

    ex41

--- chapter5.py
+++ chapter5.py
@@ -2,20 +2,28 @@ import codecs
 import sys
 import xml.etree.ElementTree as et
 
+#ex40
 class Morph:
     'surface'
     'base'
     'pos'
     'pos1'
 
+#ex41
+class Chunk:
+    "morphs"
+    "dst"
+    "srcs"
 
 tree = et.parse('neko.xml')
 root = tree.getroot()
-#ex40
+
 cnt = 0
 for stc in root.findall('sentence'):
-    morph_list = []
+    mors_in_stc = []
+    chk_in_stc = []
     for chk in stc.findall('chunk'):
+        mors_in_chk = []
         for tok in chk.findall('tok'):
             feature = tok.get('feature')
             ftrs = feature.split(',')
@@ -24,8 +32,30 @@ for stc in root.findall('sentence'):
             m.base = ftrs[6]
             m.pos = ftrs[0]
             m.pos1 = ftrs[1]
-            morph_list.append(m)
+            mors_in_stc.append(m)
+            mors_in_chk.append(m)
+
+        c = Chunk()
+        c.morphs = mors_in_chk
+        c.dst = chk.get("link")
+        c.srcs = chk.get("id")
+        chk_in_stc.append(c)
+
     cnt += 1
+
     if cnt == 3:
-        for m in morph_list:
+        for m in mors_in_stc:
             sys.stdout.buffer.write(m.surface.encode('utf-8'))
+        print()
+
+    elif cnt == 8:
+        for c in chk_in_stc:
+            for m in c.morphs:
+                sys.stdout.buffer.write(m.surface.encode('utf-8'))
+            sys.stdout.buffer.write('->'.encode('utf-8'))
+            for c2 in chk_in_stc:
+                if c2.srcs == c.dst:
+                    for m2 in c2.morphs:
+                        sys.stdout.buffer.write(m2.surface.encode('utf-8'))
+
+            print()
